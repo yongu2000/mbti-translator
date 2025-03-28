488f9ebd5a6d80b518367d5300d02116a5bdec9d
Yong U Cho <yongu2000@naver.com>
2025-03-29 02:31:14 +0900

    LoRA 학습 결과 조금 애매

--- model/src/peft_lora/test.py
+++ model/src/peft_lora/test.py
@@ -12,10 +12,23 @@ base = AutoModelForSeq2SeqLM.from_pretrained("gogamza/kobart-base-v2")
 tokenizer = AutoTokenizer.from_pretrained("gogamza/kobart-base-v2")
 model = PeftModel.from_pretrained(base, lora_path).to("cuda")
 
-input_text = "아 오늘 너무 피곤하다."
-inputs = tokenizer(input_text, return_tensors="pt").to("cuda")
-inputs.pop("token_type_ids", None)
+input_text = ["아 오늘 너무 피곤하다.", 
+              "오늘 날씨가 좋네요.",
+              "나는 게임은 별로 안 좋아해",
+        "이번 주말에 뭐하실 계획이에요?",
+        "새로운 프로젝트를 시작하게 되었어요.",
+        "점심 뭐 먹었어요?",
+        "요즘 어떻게 지내요?",
+        "나랑 영화 볼래요?",
+        "이번 시험 잘 봤어요?",
+        "취미가 뭐야?",
+        "스트레스 받을 때 어떻게 해?",
+        "좋아하는 음식이 뭐야?"]
 
-with torch.no_grad():
-    output = model.generate(**inputs, max_new_tokens=50)
-    print(tokenizer.decode(output[0], skip_special_tokens=True))
+for text in input_text:
+    inputs = tokenizer(text, return_tensors="pt").to("cuda")
+    inputs.pop("token_type_ids", None)
+
+    with torch.no_grad():
+        output = model.generate(**inputs, max_new_tokens=50)
+        print(tokenizer.decode(output[0], skip_special_tokens=True))
